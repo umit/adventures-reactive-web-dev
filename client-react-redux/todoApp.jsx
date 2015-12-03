66e904ca0580108383275853e5ebe3cfc3b8d0dc
foxdonut <fdaoud@fastmail.com>
2015-12-03 09:48:39 -0500

    Added redux-promise and redux-actions to react-redux example.

--- client-react-redux/todoApp.jsx
+++ client-react-redux/todoApp.jsx
@@ -5,11 +5,20 @@ var R = require("ramda");
 var Redux = require("redux");
 var ReactRedux = require("react-redux");
 var thunk = require("redux-thunk");
+var promiseMiddleware = require("redux-promise");
+var Actions = require("redux-actions");
 var todoListModel = require("./todoList/model");
 var TodoList = require("./todoList/component.jsx");
 
 module.exports = function(element) {
+  // 1. with redux-thunk
+  /*
   var store = Redux.applyMiddleware(thunk)(Redux.createStore)(todoListModel);
+  */
+
+  // 2. with redux-actions and redux-promise
+  var store = Redux.applyMiddleware(promiseMiddleware)(Redux.createStore)(todoListModel);
+
   var App = ReactRedux.connect(R.identity)(TodoList);
   var Provider = ReactRedux.Provider;
 
@@ -29,9 +38,18 @@ module.exports = function(element) {
     }
   };
 
-  store.dispatch(dispatch => {
+  // 1. with thunk
+  /*
+  var getTodos = function(dispatch) {
     ajax.getJSON(todoUrl.get).then(todoList => {
       dispatch({type: "EVT_LIST", payload: {todos: todoList}});
     });
-  });
+  };
+  */
+
+  // 2. with redux-actions and redux-promise
+  var getTodos = Actions.createAction("EVT_LIST",
+    () => ajax.getJSON(todoUrl.get).then(todoList => R.assoc("todos", todoList, {})));
+
+  store.dispatch(getTodos());
 };
