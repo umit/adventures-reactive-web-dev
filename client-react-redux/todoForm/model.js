eeacb6fbd88127ccafc82d74799a7e5b4948ee91
foxdonut <fdaoud@fastmail.com>
2015-12-22 15:40:45 -0500

    Handle typing after validation errors, clearing after save.

--- client-react-redux/todoForm/model.js
+++ client-react-redux/todoForm/model.js
@@ -2,13 +2,16 @@ import {assoc} from "ramda";
 
 export default function(model, action) {
   if (action) {
-    if (action.type === "ACTION_EDIT" || action.type === "ACTION_IN_FORM") {
+    if (action.type === "ACTION_EDIT") {
       return action.payload;
+    }
+    else if (action.type === "ACTION_IN_FORM") {
+      return assoc("validationErrors", model.validationErrors, action.payload);
     }
     else if (action.type === "ACTION_SAVE" && action.payload.validationErrors) {
       return assoc("validationErrors", action.payload.validationErrors, model);
     }
-    else if (action.type === "ACTION_CANCEL") {
+    else if (action.type === "ACTION_CANCEL" || action.type.endsWith("FULFILLED")) {
       return {};
     }
   }
