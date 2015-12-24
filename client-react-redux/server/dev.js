9359b06b2dcadf0c2cd94e448bb16cbfaee10db1
foxdonut <fdaoud@fastmail.com>
2015-12-24 11:25:17 -0500

    Working on webpack dev server for hot reloading.

---
+++ client-react-redux/server/dev.js
@@ -0,0 +1,16 @@
+var webpack = require("webpack");
+var WebpackDevServer = require("webpack-dev-server");
+var config = require("../webpack.config.dev");
+
+new WebpackDevServer(webpack(config), {
+  hot: true,
+  historyApiFallback: true,
+  proxy: {
+    "*": "http://localhost:3000"
+  }
+}).listen(3013, "localhost", function(err, result) {
+  if (err) {
+    console.log(err);
+  }
+  console.log("Listening at localhost:3013");
+});
