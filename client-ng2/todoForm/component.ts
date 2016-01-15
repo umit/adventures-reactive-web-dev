353663917cb50f069b78aa324b592367dee1c272
foxdonut <fdaoud@fastmail.com>
2016-01-15 08:08:18 -0500

    Simplify form by using independent validation.

--- client-ng2/todoForm/component.ts
+++ client-ng2/todoForm/component.ts
@@ -1,5 +1,5 @@
 import {Component, Inject, OnDestroy} from "angular2/core";
-import {FORM_DIRECTIVES, ControlGroup, FormBuilder, NgClass, NgForm} from "angular2/common";
+import {NgClass} from "angular2/common";
 import {Store} from "redux";
 
 import {Todo} from "../model/todo";
@@ -9,34 +9,19 @@ import {Todo} from "../model/todo";
   template: `
     <div class="row">
       <div class="col-md-4">
-        <form #todoForm="ngForm" [ngFormModel]="myForm">
+        <form>
           <input type="hidden" name="id" value={{todo.id}}/>
           <div [ngClass]="priorityClasses">
             <label for="priority">Priority:</label>
             <input type="text" id="priority" name="priority" class="form-control"
-              [(ngModel)]="todo.priority" ngControl="priority" #priority="ngForm"/>
-            <div [hidden]="!submitted || priority.valid">
-              <span *ngIf="priority.control.hasError('required')" class="help-block">
-                Priority can't be blank
-              </span>
-              <span *ngIf="priority.control.hasError('invalidNumber')" class="help-block">
-                Priority is not a number
-              </span>
-              <span *ngIf="priority.control.hasError('belowRange')" class="help-block">
-                Priority must be greater than 0
-              </span>
-              <span *ngIf="priority.control.hasError('aboveRange')" class="help-block">
-                Priority must be less than or equal to 10
-              </span>
-            </div>
+              [(ngModel)]="todo.priority"/>
+            <span class="help-block">{{validationErrors["priority"]}}</span>
           </div>
           <div [ngClass]="descriptionClasses">
             <label for="description">Description:</label>
             <input type="text" id="description" name="description" class="form-control"
-              [(ngModel)]="todo.description" ngControl="description" #description="ngForm"/>
-            <div [hidden]="!submitted || description.valid">
-              <span class="help-block">Description can't be blank</span>
-            </div>
+              [(ngModel)]="todo.description"/>
+            <span class="help-block">{{validationErrors["description"]}}</span>
           </div>
           <div>
             <button class="btn btn-primary btn-xs" (click)="onSave(todo)">Save</button>
@@ -47,31 +32,27 @@ import {Todo} from "../model/todo";
       </div>
     </div>
   `,
-  directives: [FORM_DIRECTIVES, NgClass]
+  directives: [NgClass]
 })
 export class TodoForm implements OnDestroy {
   todo: Todo = {};
+  validationErrors = {};
 
-  submitted: boolean = false;
   priorityClasses: {[s: string]: boolean} = {"form-group": true, "has-error": false};
   descriptionClasses: {[s: string]: boolean} = {"form-group": true, "has-error": false};
 
-  myForm: ControlGroup;
   unsubscribe: Function;
 
   constructor(
-    fb: FormBuilder,
     @Inject("ReduxStore") private store: Store,
     @Inject("formActions") private actions
   ) {
     this.unsubscribe = this.store.subscribe(() => {
       let state = this.store.getState();
       this.todo = state.form.todo;
-    });
-
-    this.myForm = fb.group({
-      "priority": [""],
-      "description": [""]
+      this.validationErrors = state.form.validationErrors || {};
+      this.priorityClasses["has-error"] = !!this.validationErrors["priority"];
+      this.descriptionClasses["has-error"] = !!this.validationErrors["description"];
     });
   }
 
@@ -81,10 +62,6 @@ export class TodoForm implements OnDestroy {
 
   onSave(todo: Todo) {
     this.store.dispatch(this.actions.saveTodo({todo:todo}));
-  }
-
-  onSubmit() {
-    this.submitted = true;
   }
 
   ngOnDestroy() {
