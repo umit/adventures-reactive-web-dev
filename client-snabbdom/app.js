8b9d16aae4039df3f951ad6438c65aaa6e859662
foxdonut <fdaoud@fastmail.com>
2016-02-09 23:18:24 -0500

    First example with snabbdom, most, most-subject, union-type.

--- client-snabbdom/app.js
+++ client-snabbdom/app.js
@@ -1,5 +1,9 @@
+import Type from "union-type";
+import most from "most";
+import {subject} from "most-subject";
 import snabbdom from "snabbdom";
 
+// snabbdom setup
 const patch = snabbdom.init([
   require('snabbdom/modules/props'),
   require('snabbdom/modules/eventlisteners')
@@ -7,5 +11,39 @@ const patch = snabbdom.init([
 
 const h = require("snabbdom/h");
 
+// destination node
 const appNode = document.getElementById("app");
+
+// Action union type
+const Action = Type({Increment: [Number], Decrement: [Number]});
+
+// view : Address -> Model -> VNode
+const view = address => counter => {
+  // Action handlers
+  const incrementHandler = (address, value) => address.next(Action.Increment(value));
+  const decrementHandler = (address, value) => address.next(Action.Decrement(value));
+
+  return h("div", [
+    h("span", `Counter: ${counter}`),
+    h("span", " "),
+    h("button", {on: {click: [incrementHandler, address, 2]}}, "Increment"),
+    h("span", " "),
+    h("button", {on: {click: [decrementHandler, address, 1]}}, "Decrement")
+  ])
+};
+
+// mailbox setup
+const {observer, stream} = subject(Action.Increment(0));
+
+// update function
+const update = (counter, action) => Action.case({
+  Increment: value => counter + value,
+  Decrement: value => counter - value
+}, action);
+
+// view stream
+const view$ = stream.scan(update, 13).map(view(observer));
+
+// view renderer
+view$.scan((vnode, view) => patch(vnode, view), appNode).drain();
 
