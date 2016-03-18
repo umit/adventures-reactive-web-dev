da6902bddbce3c337902d5e5cdb2788cda584753
foxdonut <fdaoud@fastmail.com>
2016-03-18 08:42:32 -0400

    Elm example with TodoList feature.

--- client-elm/TodoMain.elm
+++ client-elm/TodoMain.elm
@@ -5,20 +5,9 @@ import Effects exposing (Never)
 import Html exposing (Html, div)
 import Task exposing (Task)
 import StartApp exposing (App, start)
-import TodoForm.Action exposing (Action(Edit))
-import TodoForm.Feature exposing (createTodoFormFeature)
-import TodoForm.Model
 import TodoList.Action exposing (Action(ShowList, UpdateList))
 import TodoList.Feature exposing (createTodoListFeature)
 import TodoList.Model
-import TodoSummary.Action exposing (Action(Update, LastSaved))
-import TodoSummary.Feature exposing (createTodoSummaryFeature)
-import TodoSummary.Model
-
-
-todoFormMailbox : Signal.Mailbox TodoForm.Action.Action
-todoFormMailbox =
-  Signal.mailbox (Edit blankTodo)
 
 
 todoListMailbox : Signal.Mailbox TodoList.Action.Action
@@ -26,60 +15,34 @@ todoListMailbox =
   Signal.mailbox (ShowList TodoList.Model.initialModel)
 
 
-todoSummaryMailbox : Signal.Mailbox TodoSummary.Action.Action
-todoSummaryMailbox =
-  Signal.mailbox (Update [])
-
-
 todoListFeature : App TodoList.Model.Model
 todoListFeature =
   createTodoListFeature
     { inputs = [ todoListMailbox.signal ]
     , outputs =
-        { onEditTodo = [ Signal.forwardTo todoFormMailbox.address Edit ]
-        , onUpdatedList = [ Signal.forwardTo todoSummaryMailbox.address Update ]
+        { onEditTodo = []
+        , onUpdatedList = []
         }
     }
 
 
-todoFormFeature : App TodoForm.Model.Model
-todoFormFeature =
-  createTodoFormFeature
-    { inputs = [ todoFormMailbox.signal ]
-    , outputs =
-        { onSaveTodo =
-            [ Signal.forwardTo todoListMailbox.address UpdateList
-            , Signal.forwardTo todoSummaryMailbox.address LastSaved
-            ]
-        }
-    }
-
-
-todoSummaryFeature : App TodoSummary.Model.Model
-todoSummaryFeature =
-  createTodoSummaryFeature { inputs = [ todoSummaryMailbox.signal ] }
-
-
-todoMainView : Html -> Html -> Html -> Html
-todoMainView todoListView todoFormView todoSummaryView =
+todoMainView : Html -> Html
+todoMainView todoListView =
   div
     []
-    [ todoFormView
-    , todoListView
-    , todoSummaryView
+    [ todoListView
     ]
 
 
 html : Signal Html
 html =
-  Signal.map3 todoMainView todoListFeature.html todoFormFeature.html todoSummaryFeature.html
+  Signal.map todoMainView todoListFeature.html
 
 
 tasks : Signal (Task Never ())
 tasks =
   Signal.mergeMany
     [ todoListFeature.tasks
-    , todoFormFeature.tasks
     ]
 
 
--- client-elm/TodoSummary/Action.elm
+++
@@ -1,9 +0,0 @@
-module TodoSummary.Action (Action(..)) where
-
-import Common.Model exposing (Todo)
-
-
-type Action
-  = Update (List Todo)
-  | LastSaved (Maybe Todo)
-
