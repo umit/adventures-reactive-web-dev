8a78fa118824866a6e17805ac71062a93bd09954
foxdonut <fdaoud@fastmail.com>
2016-03-26 00:22:58 -0400

    Exploring elm arch in JS with React, RxJS, and other friends.

--- client-react-elm-arch/test/library/feature-tests.js
+++ client-react-elm-arch/test/library/feature-tests.js
@@ -1,24 +1,49 @@
 import {expect} from "chai";
 import Maybe from "data.maybe";
+import {identity, merge} from "ramda";
 
 import {createFeature} from "../../library/feature";
 
 describe("library/feature", function() {
+  const baseConfig = {
+    inputs: [],
+    initialModel: {
+      model: {},
+      task: Maybe.Nothing()
+    },
+    update: (action, model) => ({
+      model: model,
+      task: Maybe.Nothing()
+    }),
+    view: address => model => null
+  };
+
   it("should create a feature", function() {
-    const feature = createFeature({
-      inputs: [],
-      initialModel: {
-        model: {},
-        task: Maybe.Nothing()
-      },
-      update: (action, model) => ({
-        model: model,
-        task: Maybe.Nothing()
-      }),
-      view: address => model => null
-    });
+    const feature = createFeature(baseConfig);
 
     expect(feature.view$).to.exist;
     expect(feature.task$).to.exist;
+  });
+
+  it("should call the view with an address and a model", function(done) {
+    const initial = { duck: "quack" };
+
+    const feature = createFeature(merge(baseConfig, {
+      initialModel: {
+        model: initial,
+        task: Maybe.Nothing()
+      },
+      view: address => model => {
+        expect(address).to.exist;
+        expect(address.next).to.exist;
+        expect(address.next).to.be.a("function");
+
+        expect(model).to.equal(initial);
+
+        done();
+      }
+    }));
+
+    feature.view$.subscribe(identity);
   });
 });
