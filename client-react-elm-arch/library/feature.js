80834a6c3f8a7db5d9f1bacec234ee77abcb6642
foxdonut <fdaoud@fastmail.com>
2016-03-29 11:47:08 -0400

    Add js-beautify

--- client-react-elm-arch/library/feature.js
+++ client-react-elm-arch/library/feature.js
@@ -11,59 +11,59 @@ Feature =
   , taskRunner : (Task Never ())$
   }
 */
-import { BehaviorSubject } from "rxjs/subject/BehaviorSubject" ;
-import "rxjs/add/operator/map" ;
-import "rxjs/add/operator/merge" ;
-import "rxjs/add/operator/publishReplay" ;
-import "rxjs/add/operator/scan" ;
-import { Just, fromNullable } from "data.maybe" ;
-import Task from "data.task" ;
-import { identity, prop } from "ramda" ;
+import { BehaviorSubject } from "rxjs/subject/BehaviorSubject";
+import "rxjs/add/operator/map";
+import "rxjs/add/operator/merge";
+import "rxjs/add/operator/publishReplay";
+import "rxjs/add/operator/scan";
+import { Just, fromNullable } from "data.maybe";
+import Task from "data.task";
+import { identity, prop } from "ramda";
 
 const createFeature = config => {
   // action$ : Observable<Action>
-  const action$ = new BehaviorSubject(null) ;
+  const action$ = new BehaviorSubject(null);
 
   // maybeAction : Observable<Maybe Action>
-  let maybeAction$ = action$.map(fromNullable) ;
+  let maybeAction$ = action$.map(fromNullable);
 
   config.inputs.forEach(input$ => {
-    maybeAction$ = maybeAction$.merge(input$.map(Just)) ;
-  }) ;
+    maybeAction$ = maybeAction$.merge(input$.map(Just));
+  });
 
   // update : { model : Model, task : Maybe ( Task Action ) } -> Maybe Action ->
   //   { model : Model, task : Maybe ( Task Action ) }
   const update = (modelAndTask, maybeAction) => maybeAction
     .map(action => config.update(action)(modelAndTask.model))
-    .getOrElse(modelAndTask) ;
+    .getOrElse(modelAndTask);
 
   // modelAndTask$ : Observable<[Model, Maybe (Task Action)]>
-  const modelAndTask$ = maybeAction$.scan(update, config.initialModel).publishReplay(1).refCount() ;
+  const modelAndTask$ = maybeAction$.scan(update, config.initialModel).publishReplay(1).refCount();
 
   // model$ : Observable<Model>
-  const model$ = modelAndTask$.map(prop("model")) ;
+  const model$ = modelAndTask$.map(prop("model"));
 
   // view$ : Observable<Html>
-  const view$ = model$.map(config.view(action$)) ;
+  const view$ = model$.map(config.view(action$));
 
   // sendAction : Action -> Task Never ()
-  const sendAction = action => new Task((rej, res) => res(action$.next(action))) ;
+  const sendAction = action => new Task((rej, res) => res(action$.next(action)));
 
   // taskRunner$ : Observable<Task Never ()>
   const task$ = modelAndTask$.map(modelAndTask =>
     modelAndTask.task
-      .map(t => t.chain(sendAction))
-      .getOrElse(Task.of(null))
-  ) ;
+    .map(t => t.chain(sendAction))
+    .getOrElse(Task.of(null))
+  );
 
-  const result =
-    { view$
-    , task$
-    } ;
+  const result = {
+    view$,
+    task$
+  };
 
-  return result ;
-} ;
+  return result;
+};
 
-const taskRunner = task => task.fork(identity, identity) ;
+const taskRunner = task => task.fork(identity, identity);
 
-export { createFeature, taskRunner } ;
+export { createFeature, taskRunner };
--- client-react-elm-arch/test/library/feature-tests.js
+++ client-react-elm-arch/test/library/feature-tests.js
@@ -1,258 +1,255 @@
-import { expect } from "chai" ;
-import { Just, Nothing } from "data.maybe" ;
-import Task from "data.task" ;
-import { identity, inc, lensProp, merge, over } from "ramda" ;
-import { Subject } from "rxjs/Subject" ;
-import Type from "union-type" ;
+import { expect } from "chai";
+import { Just, Nothing } from "data.maybe";
+import Task from "data.task";
+import { identity, inc, lensProp, merge, over } from "ramda";
+import { Subject } from "rxjs/Subject";
+import Type from "union-type";
 
-import { createFeature, taskRunner } from "../../library/feature" ;
+import { createFeature, taskRunner } from "../../library/feature";
 
 describe("library/feature", function() {
-  const baseConfig =
-    { inputs : []
-    , initialModel :
-      { model : {}
-      , task : Nothing()
-      }
-    , update : action => model =>
-      ({ model : model
-      , task : Nothing()
-      })
-    , view : address => model => null
-    } ;
+  const baseConfig = {
+    inputs: [],
+    initialModel: {
+      model: {},
+      task: Nothing()
+    },
+    update: action => model =>
+      ({
+        model: model,
+        task: Nothing()
+      }),
+    view: address => model => null
+  };
 
   it("creates a feature", function() {
-    const feature = createFeature(baseConfig) ;
+    const feature = createFeature(baseConfig);
 
-    expect(feature.view$).to.exist ;
-    expect(feature.task$).to.exist ;
-  }) ;
+    expect(feature.view$).to.exist;
+    expect(feature.task$).to.exist;
+  });
 
   it("calls the view with an address and a model", function(done) {
-    const initial = { duck : "quack" } ;
+    const initial = { duck: "quack" };
 
-    const feature = createFeature(merge(baseConfig,
-      { initialModel :
-        { model : initial
-        , task : Nothing()
-        }
-      , view : address => model => {
-          expect(address).to.exist ;
-          expect(address.next).to.exist ;
-          expect(address.next).to.be.a("function") ;
+    const feature = createFeature(merge(baseConfig, {
+      initialModel: {
+        model: initial,
+        task: Nothing()
+      },
+      view: address => model => {
+        expect(address).to.exist;
+        expect(address.next).to.exist;
+        expect(address.next).to.be.a("function");
 
-          expect(model).to.equal(initial) ;
+        expect(model).to.equal(initial);
 
-          done() ;
-        }
-      })) ;
+        done();
+      }
+    }));
 
-    feature.view$.subscribe(identity) ;
-  }) ;
+    feature.view$.subscribe(identity);
+  });
 
   it("calls update with an action and a model", function(done) {
-    const initial = { duck : "quack" } ;
-    const testAction = "TEST" ;
-    let flag = true ;
+    const initial = { duck: "quack" };
+    const testAction = "TEST";
+    let flag = true;
 
-    const feature = createFeature(merge(baseConfig,
-      { initialModel :
-        { model : initial
-        , task : Nothing()
+    const feature = createFeature(merge(baseConfig, {
+      initialModel: {
+        model: initial,
+        task: Nothing()
+      },
+      view: address => model => {
+        if (flag) {
+          flag = false;
+          address.next(testAction);
         }
-      , view : address => model => {
-          if (flag) {
-            flag = false ;
-            address.next(testAction) ;
-          }
-        }
-      , update : action => model => {
-          expect(action).to.equal(testAction) ;
-          expect(model).to.equal(initial) ;
-          done() ;
-          return { model, task : Nothing() } ;
-        }
-      })) ;
+      },
+      update: action => model => {
+        expect(action).to.equal(testAction);
+        expect(model).to.equal(initial);
+        done();
+        return { model, task: Nothing() };
+      }
+    }));
 
-    feature.view$.subscribe(identity) ;
-  }) ;
+    feature.view$.subscribe(identity);
+  });
 
   it("sends the next action", function(done) {
-    const firstAction = "first" ;
-    const secondAction = "second" ;
-    const task = Task.of(secondAction) ;
+    const firstAction = "first";
+    const secondAction = "second";
+    const task = Task.of(secondAction);
 
-    let flag = true ;
+    let flag = true;
 
-    const feature = createFeature(merge(baseConfig,
-      { view : address => model => {
-          if (flag) {
-            flag = false ;
-            address.next(firstAction) ;
-            return "view 1" ;
-          }
-          return "view 2" ;
+    const feature = createFeature(merge(baseConfig, {
+      view: address => model => {
+        if (flag) {
+          flag = false;
+          address.next(firstAction);
+          return "view 1";
         }
-      , update : action => model => {
-          if (action === firstAction) {
-            return { model, task : Just(task) } ;
-          }
-          else if (action === secondAction) {
-            done() ;
-            return { model, task : Nothing() } ;
-          }
+        return "view 2";
+      },
+      update: action => model => {
+        if (action === firstAction) {
+          return { model, task: Just(task) };
+        } else if (action === secondAction) {
+          done();
+          return { model, task: Nothing() };
         }
-      })) ;
+      }
+    }));
 
-    feature.view$.subscribe(identity) ;
-    feature.task$.subscribe(taskRunner) ;
-  }) ;
+    feature.view$.subscribe(identity);
+    feature.task$.subscribe(taskRunner);
+  });
 
   it("updates the model", function(done) {
-    const INCREMENT = "increment" ;
+    const INCREMENT = "increment";
 
-    let flag = true ;
+    let flag = true;
 
-    const feature = createFeature(merge(baseConfig,
-      { initialModel :
-        { model : { counter : 1 }
-        , task : Nothing()
+    const feature = createFeature(merge(baseConfig, {
+      initialModel: {
+        model: { counter: 1 },
+        task: Nothing()
+      },
+      update: action => model => {
+        if (action === INCREMENT) {
+          return { model: over(lensProp("counter"), inc, model), task: Nothing() };
         }
-      , update : action => model => {
-          if (action === INCREMENT) {
-            return { model : over(lensProp("counter"), inc, model), task : Nothing() } ;
-          }
-          return { model, task : Nothing() } ;
+        return { model, task: Nothing() };
+      },
+      view: address => model => {
+        if (flag) {
+          flag = false;
+          address.next(INCREMENT);
+        } else {
+          expect(model.counter).to.equal(2);
+          done();
         }
-      , view : address => model => {
-          if (flag) {
-            flag = false ;
-            address.next(INCREMENT) ;
-          }
-          else {
-            expect(model.counter).to.equal(2) ;
-            done() ;
-          }
-        }
-      })) ;
+      }
+    }));
 
-    feature.view$.subscribe(identity) ;
-  }) ;
+    feature.view$.subscribe(identity);
+  });
 
   it("merges input signals", function(done) {
-    const INCREMENT = "increment" ;
+    const INCREMENT = "increment";
 
-    let flag = true ;
+    let flag = true;
 
-    const input = new Subject() ;
+    const input = new Subject();
 
-    const feature = createFeature(merge(baseConfig,
-      { initialModel :
-        { model : { counter : 1 }
-        , task : Nothing()
+    const feature = createFeature(merge(baseConfig, {
+      initialModel: {
+        model: { counter: 1 },
+        task: Nothing()
+      },
+      update: action => model => {
+        if (action === INCREMENT) {
+          return { model: over(lensProp("counter"), inc, model), task: Nothing() };
         }
-      , update : action => model => {
-          if (action === INCREMENT) {
-            return { model : over(lensProp("counter"), inc, model), task : Nothing() } ;
-          }
-          return { model, task : Nothing() } ;
+        return { model, task: Nothing() };
+      },
+      view: address => model => {
+        if (model.counter === 2) {
+          done();
         }
-      , view : address => model => {
-          if (model.counter === 2) {
-            done() ;
-          }
-        }
-      , inputs : [ input ]
-      })) ;
+      },
+      inputs: [input]
+    }));
 
-    feature.view$.subscribe(identity) ;
+    feature.view$.subscribe(identity);
 
-    input.next(INCREMENT) ;
-  }) ;
+    input.next(INCREMENT);
+  });
 
   it("executes tasks", function(done) {
-    const INCREMENT = "increment" ;
-    const NO_OP = "noOp" ;
+    const INCREMENT = "increment";
+    const NO_OP = "noOp";
 
-    let flag = true ;
+    let flag = true;
 
-    const input = new Subject() ;
+    const input = new Subject();
 
     const task = new Task((rej, res) => {
-      flag = false ;
-      res(NO_OP) ;
-    }) ;
+      flag = false;
+      res(NO_OP);
+    });
 
-    const feature = createFeature(merge(baseConfig,
-      { initialModel :
-        { model : { counter : 1 }
-        , task : Nothing()
+    const feature = createFeature(merge(baseConfig, {
+      initialModel: {
+        model: { counter: 1 },
+        task: Nothing()
+      },
+      update: action => model => {
+        if (action === INCREMENT) {
+          return { model: over(lensProp("counter"), inc, model), task: Just(task) };
         }
-      , update : action => model => {
-          if (action === INCREMENT) {
-            return { model : over(lensProp("counter"), inc, model), task : Just(task) } ;
-          }
-          return { model, task : Nothing() } ;
+        return { model, task: Nothing() };
+      },
+      view: address => model => {
+        if (!flag) {
+          done();
         }
-      , view : address => model => {
-          if (!flag) {
-            done() ;
-          }
-        }
-      , inputs : [ input ]
-      })) ;
+      },
+      inputs: [input]
+    }));
 
-    feature.view$.subscribe(identity) ;
-    feature.task$.subscribe(taskRunner) ;
+    feature.view$.subscribe(identity);
+    feature.task$.subscribe(taskRunner);
 
-    input.next(INCREMENT) ;
-  }) ;
+    input.next(INCREMENT);
+  });
 
   it("dispatches the next action", function(done) {
-    const input = new Subject() ;
-    const output = new Subject() ;
+    const input = new Subject();
+    const output = new Subject();
 
-    const todos =
-    [ { id : 1
-      , description : "test 1"
-      }
-    , { id : 2
-      , description : "test 2"
-      }
-    ] ;
+    const todos = [{
+      id: 1,
+      description: "test 1"
+    }, {
+      id: 2,
+      description: "test 2"
+    }];
 
-    const Action = Type
-    ( { NoOp : []
-      , LoadList : []
-      , ShowList : [Array]
-      }
-    ) ;
+    const Action = Type({
+      NoOp: [],
+      LoadList: [],
+      ShowList: [Array]
+    });
 
-    const loadListTask = new Task((rej, res) => res(Action.ShowList(todos))) ;
+    const loadListTask = new Task((rej, res) => res(Action.ShowList(todos)));
 
     const showListTask = new Task((rej, res) => {
       output.next(todos);
       res(Action.NoOp());
-    }) ;
+    });
 
     const handler = model =>
-    ( { NoOp : () => {
-          expect(model).to.deep.equal(todos) ;
-          done() ;
-          return { model, task : Nothing() } ;
-        }
-      , LoadList : () => ({ model : [], task : Just(loadListTask)})
-      , ShowList : todos => ({ model : todos, task : Just(showListTask) })
-      }
-    ) ;
+      ({
+        NoOp: () => {
+          expect(model).to.deep.equal(todos);
+          done();
+          return { model, task: Nothing() };
+        },
+        LoadList: () => ({ model: [], task: Just(loadListTask) }),
+        ShowList: todos => ({ model: todos, task: Just(showListTask) })
+      });
 
-    const feature = createFeature(merge(baseConfig,
-      { update : action => model => Action.case(handler(model), action)
-      , inputs : [ input ]
-      })) ;
+    const feature = createFeature(merge(baseConfig, {
+      update: action => model => Action.case(handler(model), action),
+      inputs: [input]
+    }));
 
-    feature.task$.subscribe(taskRunner) ;
+    feature.task$.subscribe(taskRunner);
 
-    input.next(Action.LoadList()) ;
-  }) ;
-}) ;
+    input.next(Action.LoadList());
+  });
+});
